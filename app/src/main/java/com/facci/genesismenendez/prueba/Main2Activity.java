43584433c93c46390b596613476fa9b8cc8f3c6b
jarschamaco <rjefersonandres@outlook.es>
2019-12-17 13:07:40 -0500

    1/7

--- app/src/main/java/com/facci/genesismenendez/prueba/Main2Activity.java
+++ app/src/main/java/com/facci/genesismenendez/prueba/Main2Activity.java
@@ -3,12 +3,34 @@ package com.facci.genesismenendez.prueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView ruta,origen, destino, compañia, tiempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ruta = findViewById(R.id.nameTextView);
+        origen = findViewById(R.id.origen_);
+        destino = findViewById(R.id.destino_);
+        compañia = findViewById(R.id.comp);
+        tiempo = findViewById(R.id.tiempo);
+
+        Bundle bundle = this.getIntent().getExtras();
+
+
+        ruta.setText(bundle.getString("ruta"));
+        origen.setText(bundle.getString("origen"));
+        destino.setText(bundle.getString("destino"));
+        compañia.setText(bundle.getString("company"));
+        tiempo.setText(bundle.getString("tiempo"));
+
+
+
+
     }
 }
